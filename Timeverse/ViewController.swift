2ee3a22aa1e7386d9b4488c95eef1700841fa409
Mohamed Tedjani Meftah <mohamed-ted@live.com>
2016-09-26 21:07:02 +0100

    feat: added the update loop.

--- Timeverse/ViewController.swift
+++ Timeverse/ViewController.swift
@@ -11,4 +11,26 @@ import TimeverseKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var textTime: TextTimeView!
+    
+    private var lastMinute:Int = -1;
+    private var lastHour:Int = -1;
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateTextTime), userInfo: nil, repeats: true)
+    }
+    
+    func updateTextTime () {
+        let date = Date()
+        let calendar = Calendar.current
+        let minute = Int(round(Float(calendar.component(.minute, from: date)) / 5.0) * 5)
+        let hour = calendar.component(.hour, from: date) % 12
+        
+        if (lastMinute != minute || lastHour != hour) {
+            lastMinute = minute
+            lastHour = hour
+            textTime.setTime(hour: lastHour, minute: lastMinute)
+        }
+    }
+
 }
